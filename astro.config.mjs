c02013a70028b7362c769e074c15193e8861b5b3
Saqib Safdar <saqib.safdar@gmail.com>
2024-06-15 17:29:29 +0100

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'saqibsafdar11.github.io',
+  base: '/astrofy-main'
   integrations: [mdx(), sitemap(), tailwind()]
 });
